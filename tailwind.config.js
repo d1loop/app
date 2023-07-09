3577e4d0fb9a6d08407b2bf833d4cfc396410577
Ron Nuss <shalom@itsron.space>
2023-07-09 17:03:33 +0000

    Fixed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         'main-background': 'rgb(var(--main-background) / <alpha-value>)',
         'main-search-background': 'rgb(var(--main-search-background) / <alpha-value>)',
         'main-sidebar-background': 'rgb(var(--main-sidebar-background) / <alpha-value>)',
-        'main-accent': 'rgb(var(--main-accent) / <alpha-value>)',
+        'main-accent': 'rgb(var(--accent-purple) / <alpha-value>)',
         'accent-yellow': 'rgb(var(--accent-yellow) / <alpha-value>)',
         'accent-blue': 'rgb(var(--accent-purple) / <alpha-value>)',
         'accent-pink': 'rgb(var(--accent-pink) / <alpha-value>)',
